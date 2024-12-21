6addf7dad0c36bc21843fcb0e9a37139a3ac01ca
khatrin623op <hackerop8905@gmail.com>
2024-12-21 20:12:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6992053134:AAEJo_mMoUWWYlb1Ok8hNwyLCe9FKy0MykM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@FLAMECHEATSOP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1966437076")
+
+
+
+
+
+
+
+  
